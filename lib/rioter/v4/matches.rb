7a3f72a5a2c02e452541e55384f01bad05385d73
Carlos Derich <d.erich@hotmail.com>
2019-11-22 21:11:58 -0500

    match#by_account: add opts

--- lib/rioter/v4/matches.rb
+++ lib/rioter/v4/matches.rb
@@ -16,8 +16,21 @@ module Rioter
         MatchDetail.new(match_detail)
       end
 
-      def by_account(encrypted_account_id:)
+      def by_account(encrypted_account_id, opts)
         url = "#{base_url}match/v4/matchlists/by-account/#{encrypted_account_id}"
+
+        # TODO: move to helpers
+        if opts
+          url = url + "?"
+          query_string = []
+          opts.each do |k, v|
+           query_string << "endIndex=#{v}" and next if k.eql?(:end_index)
+           query_string << "beginIndex=#{v}" and next if k.eql?(:begin_index)
+           query_string << "#{k}=#{v}"
+          end
+          url = url + query_string.join("&")
+        end
+
         matches = make_request(url)
         MatchList.new(matches)
       end
